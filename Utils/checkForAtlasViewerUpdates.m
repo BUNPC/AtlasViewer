b9839f27fd1bd77e6c898276e537d084fac0fc60
sstucker <sstucker@bu.edu>
2020-06-15 15:41:01 -0400

    checkForUpdates now opens silent browser so that code on update pages is run

--- Utils/checkForAtlasViewerUpdates.m
+++ Utils/checkForAtlasViewerUpdates.m
@@ -3,17 +3,21 @@ function checkForAtlasViewerUpdates()
 %     cfg = ConfigFileClass(); TODO implement cfg
     promptFlag = 0;
     try
+        % Open a hidden web browser 
+        [~,h] = web(url);
+        p = getParentRecursive(h);
+        p.setVisible(0);
         s = urlread(url,'timeout',2);
     catch
         % App is offline or server could not be reached
+        close(h);
         return
     end
     updateTxt = ''; % Get information about potential update from s
-    vrnnum = getVernum();  % Compare to current version and set promptFlag
+    vrnum = getVernum();  % Compare to current version and set promptFlag
     % if (vrnum < updateTxt) & (cfg.GetValue('LatestUpdateRefused') < updateTxt)
     %   promptFlag = 1;
     % end
-    
     if (promptFlag)
         choice = questdlg(['An update for AtlasViewer is available: ',...
             updateTxt,...
@@ -32,4 +36,5 @@ function checkForAtlasViewerUpdates()
                 % cfg.SetValue('LatestUpdateRefused',updateTxt); 
         end
     end
+    close(h);
 end
