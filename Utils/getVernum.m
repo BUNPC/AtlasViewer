e0442e6612b43912bb24cec0facd24400c2d2feb
Jay Dubb <jayd1860@gmail.com>
2020-08-10 22:24:58 -0400

    v2.9.1
    -- Fix broken reporting of version number

--- Utils/getVernum.m
+++ Utils/getVernum.m
@@ -1,5 +1,6 @@
 function vrnnum = getVernum()
 
-vrnnum{1} = '2.9';  % Major version number
-vrnnum{2} = '0';    % Minor version number
-vrnnum{3} = '';   % Patch #: 'p1', 'p2', etc
+vrnnum{1} = '2';   % Major version #
+vrnnum{2} = '9';   % Major sub-version #
+vrnnum{3} = '1';   % Minor version #
+vrnnum{4} = '0';   % Minor sub-version # or patch #: 'p1', 'p2', etc
