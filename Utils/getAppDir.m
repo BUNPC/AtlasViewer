cac5636c94002d80cb9d5c3bb5af62a1ba703335
Jay Dubb <jayd1860@gmail.com>
2021-07-20 22:09:00 -0400

    v2.15.0
    -- Second step in changes to make Homer3 coexist with AtlasViewer in same matlab session

--- Utils/getAppDir.m
+++ Utils/getAppDir.m
@@ -1,4 +1,26 @@
-function dirname = getAppDir(isdeployed_override)
+function [out1] = getAppDir(inp1)
+out1 = [];
+ns = getNamespace();
+if isempty(ns)
+    return;
+end
+if strcmp(ns, 'AtlasViewerGUI')
+    if nargin == 0
+        [out1] = getAppDir_AtlasViewerGUI();
+    elseif nargin == 1
+        [out1] = getAppDir_AtlasViewerGUI(inp1);
+    end
+elseif strcmp(ns, 'Homer3')
+    if nargin == 0
+        [out1] = getAppDir_Homer3();
+    elseif nargin == 1
+        [out1] = getAppDir_Homer3(inp1);
+    end
+end
+
+
+% ---------------------------------------------------------
+function [dirname] = getAppDir_AtlasViewerGUI(isdeployed_override)
 
 if ~exist('isdeployed_override','var')
     isdeployed_override = 'notdeployed';
@@ -22,5 +44,36 @@ dirname(dirname=='\') = '/';
 if dirname(end) ~= '/'
     dirname(end+1) = '/';
 end
+
+
+
+
+
+% ---------------------------------------------------------
+function [dirname] = getAppDir_Homer3(isdeployed_override)
+
+if ~exist('isdeployed_override','var')
+    isdeployed_override = 'notdeployed';
+end
+
+if isdeployed() || strcmp(isdeployed_override, 'isdeployed')
+    if ispc()
+        dirname = 'c:/users/public/homer3/';
+    else
+        currdir = pwd;
+        cd ~/;
+        dirnameHome = pwd;
+        dirname = [dirnameHome, '/homer3/'];
+        cd(currdir);
+    end
+else
+    dirname = fileparts(which('Homer3.m'));
+end
+
+dirname(dirname=='\') = '/';
+if dirname(end) ~= '/'
+    dirname(end+1) = '/';
+end
+
 
 
