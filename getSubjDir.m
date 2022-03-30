e3fcfad75523aed2ff7d83c9f4d07e7dd504d883
jayd1860 <jdubb@bu.edu>
2022-03-29 21:44:49 -0400

    v2.19.2
    -- Fix issues with building and running AtlasViewer executable.

--- getSubjDir.m
+++ getSubjDir.m
@@ -1,28 +1,17 @@
 function dirname = getSubjDir(arg)
-
-if ~exist('arg','var') || isempty(arg)
-    arg = {''};
-end
-
-if length(arg) > 1
+dirname = pwd;
+if length(arg) > 0
     dirname = arg{1};
 else    
     % After checking all the above for insications of subject folder
     % see if dirname is etill empty. If it is ask user for subject dir. 
-    if isSubjDir(pwd)
-        dirname = pwd;
-    else
+    if ~isSubjDir(dirname)
         pause(.1);
-        dirname = uigetdir(pwd, 'Please select subject folder');
+        dirname = uigetdir(dirname, 'Please select subject folder');
         if dirname==0
             dirname = pwd;
-            return;
         end
     end
-end
-
-if isempty(dirname) | dirname==0
-    return;
 end
 dirname = filesepStandard(dirname);
 
