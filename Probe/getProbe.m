7088e7ea5e3754ddaeab2e93699454327b3c178b
jayd1860 <jdubb@bu.edu>
2023-03-07 12:37:07 -0500

    v2.30.1
    -- In an empty folder we should not be getting warning about missing measurement list since there's no probe to speak of

--- Probe/getProbe.m
+++ Probe/getProbe.m
@@ -315,7 +315,9 @@ end
 function probe = checkMeasList(probe)
 global logger
 logger = InitLogger(logger);
-
+if probe.isempty(probe)
+    return;
+end
 while isempty(probe.ml)
     msg{1} = sprintf('WARNING: measurement list missing from probe. Without a measurement list you can still ');
     msg{2} = sprintf('register the existing probe and run Monte Carlo but you will not be able to generate a ');
