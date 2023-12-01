293e1aea9e16ac127eb58d52799a7abb75283e9f
Meryem Ayse Yucel <49535526+mayucel@users.noreply.github.com>
2023-12-01 09:02:32 -0500

    Update showReducedMesh.m
    Previously, if the user opted for the original mesh, it was still reducing the mesh. Cleaned up to code to provide both options (reduced and orig mesh).

--- Utils/showReducedMesh.m
+++ Utils/showReducedMesh.m
@@ -8,39 +8,35 @@ function [nodeX, nNode, mesh] = showReducedMesh(mesh_orig, mesh_reduced)
 fv = mesh_orig;
 fvn = mesh_reduced;
 
-mesh = fvn;
+
+
+hf = figure;
+% Plot the original
+subplot(1,2,1)
+h=trisurf( fv.faces, fv.vertices(:,1), fv.vertices(:,2), fv.vertices(:,3) );
+title( sprintf(' Original with %d faces', size(fv.faces,1)) )
+set(h,'linestyle','none')
+light
+
+% Plot the reduced
+subplot(1,2,2)
+h=trisurf( fvn.faces, fvn.vertices(:,1), fvn.vertices(:,2), fvn.vertices(:,3) );
+title( sprintf(' New with %d faces', size(fvn.faces,1)) )
+set(h,'linestyle','none')
+light
 
 q = MenuBox( sprintf('The original pial surface has %d faces. It is recommended that this be less than 40,000 for best performance.\nShall I reduce this?',size(fv.faces,1)),{'Yes','No'});
+
 if q==1
-    hf = figure;
-    
-    % Plot the original
-    subplot(1,2,1)
-    h=trisurf( fv.faces, fv.vertices(:,1), fv.vertices(:,2), fv.vertices(:,3) );
-    title( sprintf(' Original with %d faces', size(fv.faces,1)) )
-    set(h,'linestyle','none')    
-    light
-    
-    % Plot the reduced
-    subplot(1,2,2)
-    h=trisurf( fvn.faces, fvn.vertices(:,1), fvn.vertices(:,2), fvn.vertices(:,3) );
-    title( sprintf(' New with %d faces', size(fvn.faces,1)) )
-    set(h,'linestyle','none')
-    light
-    
-    q = MenuBox('Accept this?',{'Yes','No'});
-    if q==2
-        q = MenuBox('Proceed with the original mesh? This could take a long time.',{'Yes','No'});
-        if q==1
-            mesh = fv;
-        end
-    end
-    
-    close(hf);
+    mesh = fvn;
+else
+    mesh = fv;
 end
 
-nodeX = mesh.vertices;
-elem  = mesh.faces; 
-nNode = size(nodeX,1);
+close(hf);
 
+
+nodeX = mesh.vertices;
+elem  = mesh.faces;
+nNode = size(nodeX,1);
 
